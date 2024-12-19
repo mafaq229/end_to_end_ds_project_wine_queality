ae6906ecabb79c6b5740e26826907a40ca538cd3
Muhammad Afaq <muhammadafaq1999@gmail.com>
2024-12-19 13:58:45 +0400

    Data Validation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from src.wine_quality_ds_project import logger
 from src.wine_quality_ds_project.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.wine_quality_ds_project.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -7,6 +9,17 @@ try:
     logger.info(f">>> stage {STAGE_NAME} started <<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.initiate_data_ingestion()
+    logger.info(f">>> stage {STAGE_NAME} completed <<<\n\nx=====x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logger.info(f">>> stage {STAGE_NAME} started <<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.initiate_data_validation()
     logger.info(f">>> stage {STAGE_NAME} completed <<<\n\nx=====x")
 except Exception as e:
     logger.exception(e)
--- src/wine_quality_ds_project/config/configuration.py
+++ src/wine_quality_ds_project/config/configuration.py
@@ -1,6 +1,6 @@
 from src.wine_quality_ds_project.constants import *
 from src.wine_quality_ds_project.utils.common import read_yaml, create_directories
-from src.wine_quality_ds_project.entity.config_entity import (DataIngestionConfig)
+from src.wine_quality_ds_project.entity.config_entity import (DataIngestionConfig, DataValidationConfig)
 
 
 class ConfigurationManager:
@@ -22,4 +22,18 @@ class ConfigurationManager:
             unzip_dir=config.unzip_dir
         )
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+        
+        create_directories([config.root_dir])
+        
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir=config.unzip_data_dir,
+            all_schema=schema
+        )
+        return data_validation_config
     

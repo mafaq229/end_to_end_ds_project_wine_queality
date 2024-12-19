02095b9238bf5a637b70ab43afb1cae05fee0575
Muhammad Afaq <muhammadafaq1999@gmail.com>
2024-12-19 15:19:25 +0400

    Model Training Pipeline

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.wine_quality_ds_project import logger
 from src.wine_quality_ds_project.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.wine_quality_ds_project.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.wine_quality_ds_project.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.wine_quality_ds_project.pipeline.model_training_pipeline import ModelTrainerTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -32,6 +33,17 @@ try:
     logger.info(f">>> stage {STAGE_NAME} started <<<")
     data_validation = DataTransformationTrainingPipeline()
     data_validation.initiate_data_transformation()
+    logger.info(f">>> stage {STAGE_NAME} completed <<<\n\nx=====x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Trainer Stage"
+
+try:
+    logger.info(f">>> stage {STAGE_NAME} started <<<")
+    model_trainer = ModelTrainerTrainingPipeline()
+    model_trainer.initiate_model_training()
     logger.info(f">>> stage {STAGE_NAME} completed <<<\n\nx=====x")
 except Exception as e:
     logger.exception(e)
